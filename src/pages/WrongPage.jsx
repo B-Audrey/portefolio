91557bded72cdfc5b812b9e52188c60a5640885e
B-Audrey <bonis.audreyy@gmail.com>
2023-07-04 09:54:58 +0200

    page 404

--- src/pages/WrongPage.jsx
+++ src/pages/WrongPage.jsx
@@ -5,7 +5,7 @@ const WrongPage = () => {
         return (
             <>
             <div className='error'>
-            <h1 className='error__code'><i className="fa-solid fa-meteor"></i>&nbsp;404&nbsp;<i className="fa-solid fa-meteor"></i></h1>
+            <h1 className='error__code'><i className="fa-solid fa-circle-xmark"></i>&nbsp;404&nbsp;<i className="fa-solid fa-circle-xmark"></i></h1>
             <p className='error__text'>Oups! mauvaise route 😱</p>
             <Link className='error__link' to="portefolio/">Retourner sur la page d'accueil</Link>
             </div>
